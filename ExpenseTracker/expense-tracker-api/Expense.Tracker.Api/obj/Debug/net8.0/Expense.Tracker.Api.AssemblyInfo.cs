e8c153505f596b36c20a84666860d89206e1e550
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-09-14 18:55:24 +0530

    changed some gitignore settings

--- ExpenseTracker/expense-tracker-api/Expense.Tracker.Api/obj/Debug/net8.0/Expense.Tracker.Api.AssemblyInfo.cs
+++ ExpenseTracker/expense-tracker-api/Expense.Tracker.Api/obj/Debug/net8.0/Expense.Tracker.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense.Tracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0791a264b8951317312b6c0400b51cea6b242e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a91fc6b804ab97ebfa307d200a9872b8acdee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense.Tracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense.Tracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
